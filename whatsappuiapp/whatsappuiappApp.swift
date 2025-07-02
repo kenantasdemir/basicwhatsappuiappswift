cf017ee24e563f6e0d6979e837651afc0b2a02c0
Kenan <ktasdemiriletisim@gmail.com>
2025-07-03 00:27:38 +0300

    Initial Commit

---
+++ whatsappuiapp/whatsappuiappApp.swift
@@ -0,0 +1,17 @@
+//
+//  whatsappuiappApp.swift
+//  whatsappuiapp
+//
+//  Created by kenan on 7/3/25.
+//
+
+import SwiftUI
+
+@main
+struct whatsappuiappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ whatsappuiappTests/whatsappuiappTests.swift
@@ -0,0 +1,17 @@
+//
+//  whatsappuiappTests.swift
+//  whatsappuiappTests
+//
+//  Created by kenan on 7/3/25.
+//
+
+import Testing
+@testable import whatsappuiapp
+
+struct whatsappuiappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
